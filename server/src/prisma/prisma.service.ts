c18f4df4db6b8c178fe2a968674f37bad968c53c
raeemu <suerbay55@gmail.com>
2026-05-14 14:19:54 +0300

    refactor: update Prisma dependencies and restructure Prisma service - Downgraded Prisma client and related packages to version 6.19.3. - Removed unnecessary prisma.config.ts file. - Updated schema.prisma to use environment variable for database URL. - Integrated PrismaModule into AppModule. - Refactored PrismaService to remove adapter and added shutdown hooks for graceful app termination.

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,18 +1,15 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    constructor() {
-        const adapter = new PrismaPg({
-            connectionString: process.env.DATABASE_URL!,
-        });
-
-        super({ adapter });
-    }
-
     async onModuleInit() {
         await this.$connect();
+    }
+
+    async enableShutdownHooks(app: INestApplication) {
+        process.on('beforeExit', async () => {
+            await app.close();
+        });
     }
 }
